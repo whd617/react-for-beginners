7a8cf43601336021fdb52beee87c53622fbfaf8a
Jonghyeon Choi <whd617@naver.com>
2024-04-17 16:39:18 +0900

    chore/resetting

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/hello" element={<h1>Hello!</h1>}></Route>
-        <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path="/" element={<Home />}></Route>
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        ></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
       </Routes>
     </Router>
   );
